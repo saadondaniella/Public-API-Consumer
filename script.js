8ec8fd9020856cef5cf68c26219f75a6ba74b535
Daniella Saadon <saadondaniella@gmail.com>
2025-12-04 14:25:43 +0100

    startade upp filerna, satte en css

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hämta element från HTML
+const languageSelect = document.getElementById("language");
+const customWordInput = document.getElementById("custom-word");
+const randomBtn = document.getElementById("random-btn");
+
+const wordEl = document.getElementById("word");
+const phoneticEl = document.getElementById("phonetic");
+const definitionEl = document.getElementById("definition");
+
+console.log("World of Words JS laddat ✅");
